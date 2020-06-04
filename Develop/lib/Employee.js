2431d6a5e8e87167d6f335fe702f1f913c375071
Garrett <goodbyetonto@gmail.com>
2020-06-04 15:32:31 -0700

    Have parent class and sub-classes template built

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name; 
+        this.id = id; 
+        this.email = email; 
+    }
+
+    getName() {
+
+    }
+
+    getId() {
+
+    }
+
+    getEmail() {
+
+    }
+
+    getRole(type = 'Employee') {
+        return type; 
+    }
+}
+
+module.exports = Employee; 
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,32 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee"); 
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        this.name = name; 
+        this.id = id; 
+        this.email = email; 
+        this.github = github;   
+    }
+
+    getName() {
+
+    }
+
+    getId() {
+
+    }
+
+    getEmail() {
+
+    }
+
+    getGithub() {
+
+    }
+
+    getRole(type = 'Engineer') {
+        return type; 
+    }
+}
