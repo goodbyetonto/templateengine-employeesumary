1bfa6ad42be89cd27501e44012b495a8de6b33e9
Garrett <goodbyetonto@gmail.com>
2020-06-11 17:24:15 -0700

    Working on adding as many comments as I can in app.js to demonstrate a clear understanding of the code

--- Develop/app.js
+++ Develop/app.js
@@ -1,17 +1,27 @@
+// Access to all sub classes to parent class 'Employee'
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+// Access to necessary node modules
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+// Access to .js file handling html rendering
 const render = require("./lib/htmlRenderer");
 
+
+// Assign absolute path to directory 'output', assign to OUTPUT_DIR
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+
+// Join the filename 'team.html' to OUTPUT_DIR
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+// Define async function 'collectInputs', with one parameter 'inputs', an empty array
 const collectInputs = async (inputs = []) => {
 
+    // Define 'questions', an array of objects, to be used later as inquirer prompt questions
     const questions = [
 
         {
@@ -29,11 +39,13 @@ const collectInputs = async (inputs = []) => {
             type: "input",
             name: "name",
             message: "What is this employee's name?",
+
+            // Check to make sure user didn't enter a blank string as a response
             validate: answer => {
                 if (answer !== '') {
                     return true;
                 }
-                return 'Please enter an actual name'; 
+                return "You did not enter any text. Please enter the name of this employee."; 
             }
         },
 
@@ -41,6 +53,8 @@ const collectInputs = async (inputs = []) => {
             type: "input",
             name: "id",
             message: "What is their ID number?", 
+
+            // Check to make sure user entered a numeric answer, not including alpha characters
             validate: answer => {
                 const pass = answer.match(
                     /^[1-9]\d*$/
@@ -48,7 +62,7 @@ const collectInputs = async (inputs = []) => {
                 if (pass) {
                     return true;
                 }
-                return 'Please enter an actual ID#';
+                return "You entered alpha characters. Please enter an ID#, containing only numeric characters";
             }
         },
 
@@ -56,6 +70,8 @@ const collectInputs = async (inputs = []) => {
             type: "input",
             name: "email",
             message: "What is their email address", 
+
+            // Check that email address follows abc@abc.com convention
             validate: answer => {
                 const pass = answer.match(
                     /\S+@\S+\.\S+/
@@ -63,7 +79,7 @@ const collectInputs = async (inputs = []) => {
                 if (pass) {
                     return true;
                 }
-                return 'Please enter an actual email address';
+                return "You did not enter a valid email address. Please enter a valid email address (ie. ___@___.___";
             }
         },
 
@@ -71,24 +87,27 @@ const collectInputs = async (inputs = []) => {
             type: "input", 
             name: "officeNumber", 
             message: "What is your office number?", 
-            when: (questions) => questions.empType === 'Manager'
+
+            // The 'when' property is used to prompt with this message only when 'questions.empType' === "Manager"
+            when: (questions) => questions.empType === "Manager"
         },
 
         {
             type: "input", 
             name: "ghUn", 
             message: "What is your Github Username?", 
-            when: (questions) => questions.empType === 'Engineer'
+            when: (questions) => questions.empType === "Engineer"
         },
 
         {
             type: "input", 
             name: "school", 
             message: "What University will you be graduating from?", 
-            when: (questions) => questions.empType === 'Intern'
+            when: (questions) => questions.empType === "Intern"
         },
 
         {
+            // Prompt the user if they would like to add another employee
             type: 'confirm',
             name: 'again',
             message: 'Enter another input? ',
@@ -96,7 +115,13 @@ const collectInputs = async (inputs = []) => {
         }
     ];
 
+    // define object { again, ...answers } to take in the answers from the inquirer prompt, where the first value
+    // is the boolean returned from the 'confirm' prompt
+    // As 'questions' is an async function, this script will not proceed further until this object receives its values from 
+    // the 'questions' prompt 
     const { again, ...answers } = await inquirer.prompt(questions);
+
+    
     const newInputs = [...inputs, answers];
     return again ? collectInputs(newInputs) : newInputs;
 };
@@ -125,38 +150,10 @@ const init = async () => {
 
     const employeeHtml = render(employees);
 
-    fs.writeFile(outputPath, employeeHtml, err => err ? console.log("HTML was rendered!") : console.log("There was an error during HTML rendering!")); 
-
-    // fs.writeFile(outputPath, html, err => err ? console.log("Error in rendering HTML!") : console.log("HTML was rendered!")); 
-    // fs.writeFile("engineers.json",  JSON.stringify(engineers, null, ' '), err => err ? console.log("Error in writing engineers.json") : console.log("engineers.json written")); 
-    // fs.writeFile("interns.json", JSON.stringify(interns, null, ' '), err => err ? console.log("Error in writing interns.json") : console.log("interns.json written")); 
-    // fs.writeFile("manager.json", JSON.stringify(managers, null, ' '), err => err ? console.log("Error in writing manager.json") : console.log("manager.json written"));
-    
+    fs.writeFile(outputPath, employeeHtml, err => !err ? console.log("HTML was rendered!") : console.log("There was an error during HTML rendering!"));     
 };
 
 init();
 
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work 
